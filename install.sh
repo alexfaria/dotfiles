59de695f996bb85a7ae0a3756a8843b0fa7ad230
Alex Faria <simaofaria11@gmail.com>
2016-03-07 15:18:48 +0000

    added plugin and theme

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux/tmux.conf bash_aliases vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+# olddir=~/dotfiles_old             # old dotfiles backup directory
+# files="tmux/tmux.conf bash_aliases vimrc vim zsh/zshrc zsh/oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -23,12 +23,17 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+# for file in $files; do
+#     echo "Moving any existing dotfiles from ~ to $olddir"
+#     mv ~/.$file ~/dotfiles_old/
+#     echo "Creating symlink to $file in home directory."
+#     ln -s $dir/$file ~/.$file
+# done
+ln -s $dir/vimrc ~/.vimrc
+ln -s $dir/vim ~/.vim
+ln -s $dir/bash_aliases ~/.bash_aliases
+ln -s $dir/tmux/tmux.conf ~/.tmux.conf
+ln -s $dir/zsh/zshrc ~/.zshrc
 
 echo "Make vim backup folder"
 mkdir ~/.vim./.tmp
