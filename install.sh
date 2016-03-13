caa10edefbe520e90fcf9b0ef4985806972d535d
Alex Faria <simaofaria@hotmail.com>
2016-03-13 13:41:01 +0000

    check if vim/.tmp exists

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@
 dir=~/.dotfiles                    # dotfiles directory
 cd $dir
 git submodule update --init --recursive
+
 cd $HOME
-# ln -s $dir/vimrc ~/.vimrc
-# ln -s $dir/vim ~/.vim
-# ln -s $dir/bash_aliases ~/.bash_aliases
-# ln -s $dir/tmux/tmux.conf ~/.tmux.conf
-# ln -s $dir/zsh/zshrc ~/.zshrc
 ln -s $dir/vimrc .vimrc
 ln -s $dir/vim .vim
 ln -s $dir/bash_aliases .bash_aliases
@@ -17,6 +13,9 @@ ln -s $dir/tmux/tmux.conf .tmux.conf
 ln -s $dir/zsh/zshrc .zshrc
 
 echo "Make vim backup folder"
-mkdir $dir/vim/.tmp
+if [! -d "$dir/vim/.tmp"]; then
+    mkdir $dir/vim/.tmp
+fi
+
 echo "Install vim plugins"
 vim +PluginInstall +qall
