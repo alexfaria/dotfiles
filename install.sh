97d7d773a2a3e28da3054422c29cf82afce24152
Alex Faria <simaofaria11@gmail.com>
2016-03-13 13:31:48 +0000

    update symlink creation

--- install.sh
+++ install.sh
@@ -2,12 +2,19 @@
 
 ########## Variables
 dir=~/.dotfiles                    # dotfiles directory
-
-ln -s $dir/vimrc ~/.vimrc
-ln -s $dir/vim ~/.vim
-ln -s $dir/bash_aliases ~/.bash_aliases
-ln -s $dir/tmux/tmux.conf ~/.tmux.conf
-ln -s $dir/zsh/zshrc ~/.zshrc
+cd $dir
+git submodule update --init --recursive
+cd $HOME
+# ln -s $dir/vimrc ~/.vimrc
+# ln -s $dir/vim ~/.vim
+# ln -s $dir/bash_aliases ~/.bash_aliases
+# ln -s $dir/tmux/tmux.conf ~/.tmux.conf
+# ln -s $dir/zsh/zshrc ~/.zshrc
+ln -s $dir/vimrc .vimrc
+ln -s $dir/vim .vim
+ln -s $dir/bash_aliases .bash_aliases
+ln -s $dir/tmux/tmux.conf .tmux.conf
+ln -s $dir/zsh/zshrc .zshrc
 
 echo "Make vim backup folder"
 mkdir $dir/vim/.tmp
