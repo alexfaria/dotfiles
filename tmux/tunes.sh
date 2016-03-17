13fbf0e8bec2288129b565a5dbbd877c9af728a7
Alex Faria <simaofaria@hotmail.com>
2016-03-17 21:43:46 +0000

    update tune script

--- tmux/tunes.sh
+++ tmux/tunes.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 if [ "$(pidof rhythmbox)" ]; then
     music=$(rhythmbox-client --print-playing)
-    echo "♫ $music"
+    music="♫ $music"
+    LEN=$(echo ${#music})
+    MAX=31
+    if [ $LEN -lt $MAX ]; then
+        echo $music
+    else
+        echo "${music:0:$(echo $MAX-3)} ..."
+    fi
 fi
