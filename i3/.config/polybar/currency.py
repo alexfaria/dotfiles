9504e8d0019b4dd16784926337cb72d14450c3d8
Alex Faria <simaofaria11@gmail.com>
2017-09-21 20:06:29 +0100

    update currency script

--- i3/.config/polybar/currency.py
+++ i3/.config/polybar/currency.py
@@ -2,7 +2,8 @@
 
 import requests
 
-r = requests.get('http://api.fixer.io/latest');
+# r = requests.get('http://api.fixer.io/latest');
+r = requests.get('http://api.fixer.io/latest?base=GBP');
 ticker = r.json()
 
-print('%.4f' % float(ticker['rates']['GBP']))
+print('%.4f' % float(ticker['rates']['EUR']))
