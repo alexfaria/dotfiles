c784a3f04f091627612008235abaee1768800ead
Alex Faria <afaria@student.uc.pt>
2017-09-21 10:01:33 +0100

    add scripts

---
+++ bin/bin/volume_notification.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+vol=$(amixer get Master | grep % | cut -d ' ' -f7 | cut -c 2- | cut -d ']' -f1 | head -n1)
+
+if amixer get Master | grep -q "off"; then
+  dunstify -r 999 -u critical -t 1 "Volume: $vol (Muted)"
+else
+  dunstify -r 999 -u normal -t 1 "Volume: $vol"
+fi
