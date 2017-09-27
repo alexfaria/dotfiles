ff3edf5ca32c5ec070b2e492477682526fa28ef3
Alex Faria <simaofaria11@gmail.com>
2017-09-27 12:58:30 +0100

    add conky cfg

---
+++ conky/.config/conky/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q conky
+
+# Wait until the processes have been shut down
+while pgrep -x conky >/dev/null; do sleep 1; done
+
+conky
+
+echo "conky launched..."
